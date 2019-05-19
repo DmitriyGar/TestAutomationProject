2fb59fd26004d100784f0da382d191cdc47339b1
Dmytro Harus <dharu@softserveinc.com>
2019-05-19 14:06:54 +0300

    add pages

---
+++ TestAutomationProject/PageObject/Pages.cs
@@ -0,0 +1,22 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestAutomationProject.PageObject
+{
+    class Pages
+    {
+        public MainPage MainPage { get; }
+        public SignInBlock SignInBlock { get; }
+        public Pages(IWebDriver driver)
+        {
+            if (MainPage == null)
+                MainPage = new MainPage(driver);
+            if (SignInBlock == null)
+                SignInBlock = new SignInBlock(driver);
+        }
+    }
+}
