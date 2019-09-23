1297fe53839c1783bf84bd495eedd37ed0fe1d65
Dmitriy Garus <dimitrobase@gmail.com>
2019-09-23 12:35:52 -0700

    test4

---
+++ PageObject/BaseObjects.cs
@@ -0,0 +1,16 @@
+﻿using OpenQA.Selenium;
+
+namespace TestAutomationProject.PageObject
+{
+    class BaseObjects
+    {
+        protected BaseObjects(IWebDriver driver)
+        {
+            Driver = driver;
+
+        }
+        protected readonly IWebDriver Driver;
+
+               
+    }
+}
